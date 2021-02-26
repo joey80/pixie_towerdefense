70bf845a906589a57fc96dc908fb0942e215d501
Joey Leger <joseph.leger@duke-energy.com>
2021-02-25 22:26:21 -0500

    feat: added GameObject, Scene, Level classes

---
+++ src/classes/Engine/GameObject.ts
@@ -0,0 +1,27 @@
+import * as PIXI from 'pixi.js';
+
+type GameObjectType = {
+  height: number;
+  width: number;
+};
+
+interface GameObject extends GameObjectType {}
+
+class GameObject implements GameObjectType {
+  constructor({ height, width }: GameObjectType) {
+    this.height = height;
+    this.width = width;
+  }
+
+  init() {
+    return new PIXI.Application({
+      backgroundColor: 0xffffff,
+      height: this.height,
+      resolution: devicePixelRatio,
+      width: this.width,
+    });
+  }
+}
+
+export type { GameObject as GameObjectType };
+export default GameObject;
---
+++ src/classes/Engine/Scene.ts
@@ -0,0 +1,26 @@
+import * as PIXI from 'pixi.js';
+
+type SceneType = {
+  app: PIXI.Application;
+  tick: number;
+};
+
+interface Scene extends SceneType {}
+
+class Scene implements SceneType {
+  constructor(app: SceneType['app']) {
+    this.app = app;
+    this.tick = 0;
+  }
+
+  init() {
+    this.app.ticker.add(delta => this.update(delta));
+  }
+
+  update(delta: number) {
+    this.tick = delta;
+  }
+}
+
+export type { SceneType };
+export default Scene;
