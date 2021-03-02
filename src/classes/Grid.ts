ac7a916d4292c8d0fa147861813ebeccd5bbc254
Joey Leger <joseph.leger@duke-energy.com>
2021-03-01 22:42:29 -0500

    feat: gameGrid is now rendering

--- src/classes/Grid.ts
+++ src/classes/Grid.ts
@@ -1,16 +1,35 @@
+import { Container } from 'pixi.js';
+import Cell from './Cell';
+
 type GridType = {
+  cellGap: number;
+  gameGrid: Array<Cell>;
+  height: number;
+  objectSize: number;
   size: number;
-  x: number;
-  y: number;
+  width: number;
 };
 
 interface Grid extends GridType {}
 
 class Grid implements GridType {
-  constructor({ size, x, y }: GridType) {
+  constructor({ height, size, width }: Pick<GridType, 'height' | 'size' | 'width'>) {
+    this.cellGap = 3;
+    this.height = height;
+    this.gameGrid = [];
+    this.objectSize = size - 3 * 2;
     this.size = size;
-    this.x = x;
-    this.y = y;
+    this.width = width;
+  }
+
+  init() {
+    const container = new Container();
+    for (let y = this.size; y < this.height; y += this.size) {
+      for (let x = 0; x < this.width; x += this.size) {
+        container.addChild(new Cell({ size: this.size, x, y }).init());
+      }
+    }
+    return container;
   }
 }
 
